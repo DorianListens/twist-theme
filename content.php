b3236b87ce98f054bb4972e4c3c2d858bcbc6952
Dorian <dorian.scheidt@gmail.com>
2013-12-07 00:47:04 -0500

    Footer Style and Centered links

--- content.php
+++ content.php
@@ -21,7 +21,7 @@
 			<?php if ( is_single() ) : ?>
 			<h1 class="entry-title"><?php the_title(); ?></h1>
 			<?php else : ?>
-			<h1 class="entry-title">
+			<h1 class="fp-entry-title">
 				<a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a>
 			</h1>
 			<?php endif; // is_single() ?>
